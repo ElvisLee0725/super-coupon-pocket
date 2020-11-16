1fae3ec507734bf7181649f46fc1c96fee35b739
ElvisLee0725 <ElvisLee0725@gmail.com>
2020-11-16 05:15:51 +0000

    Create new sql for ranking

--- client/actions/history.js
+++ client/actions/history.js
@@ -5,9 +5,10 @@ export const getPersonalHistory = () => async dispatch => {
   try {
     const resAll = await axios.get('/api/users/history-all');
     const resUsed = await axios.get('/api/users/history-all-used');
+    const ranking = await axios.get('/api/users/ranking');
     dispatch({
       type: GET_PERSONAL_HISTORY,
-      payload: { ...resAll.data, ...resUsed.data }
+      payload: { ...resAll.data, ...resUsed.data, ...ranking.data }
     });
   } catch (err) {
     dispatch({
