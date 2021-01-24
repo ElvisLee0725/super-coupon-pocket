76c04aebcce6a4db9e49c65a8bb48158882d9be6
ElvisLee0725 <ElvisLee0725@gmail.com>
2021-01-24 01:12:59 +0000

    Added instruction on md to lg screen and a link to open on small screen

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -29,53 +29,93 @@ const Login = ({ login, guestLogin, isAuthenticated }) => {
 
   return (
     <Fragment>
-      <div className='box-layout'>
-        <div className='box-layout__box'>
-          <img
-            src='https://ubuntu-ec2-s3.s3-us-west-1.amazonaws.com/scp/images/scp-logo.png'
-            className='img-fluid mb-5'
-            alt='SCP Logo'
-          />
+      <div className='container'>
+        <div className='row'>
+          <div className='d-none d-md-block col-md-6'>
+            <div className='box-layout'>
+              <div>
+                <h1>Introduction</h1>
+                <p className='lead text-justify'>Have you ever got a coupon from one of your favorite stores, and you are excited to use it next time when you have a chance, then, you just totally forget about it?
+              What’s even worse is, when you finally recall you got that coupon, it’s after you made your purchase!
+              So here comes the Super Coupon Pocket. With this app, you can record all those coupons you ever received and you can easily search and see when it will expire. Just create an account and start saving today!</p>
+              </div>
+            </div>
+          </div>
+          <div className='col-12 col-md-6'>
+            <div className='box-layout'>
+              <div className='box-layout__box'>
+                <a className='d-block d-md-none mb-3' data-toggle="modal" data-target="#appIntro">
+                  <u>What is this?</u>
+                </a>
+                <div className="modal fade" id="appIntro" tabIndex="-1" role="dialog" aria-labelledby="appIntro" aria-hidden="true">
+                  <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                      <div className="modal-header">
+                        <h5 className="modal-title" id="appIntro">Introduction</h5>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div className="modal-body">
+                        <p className='lead text-justify'>Have you ever got a coupon from one of your favorite stores, and you are excited to use it next time when you have a chance, then, you just totally forget about it?
+                    What’s even worse is, when you finally recall you got that coupon, it’s after you made your purchase!
+                    So here comes the Super Coupon Pocket. With this app, you can record all those coupons you ever received and you can easily search and see when it will expire. Just create an account and start saving today!</p>
+                      </div>
+                      <div className="modal-footer">
+                        <button type="button" className="btn btn-themeBlue" data-dismiss="modal">Close</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
 
-          <form onSubmit={e => handleSubmit(e)}>
-            <div className='form-group form-group--insertIcon insertIcon-left'>
-              <input
-                type='email'
-                className='form-control input-field font-italic'
-                placeholder='Email'
-                name='email'
-                value={email}
-                onChange={e => handleChange(e)}
-                required
-              />
-              <i className='far fa-envelope'></i>
+                <img
+                  src='https://ubuntu-ec2-s3.s3-us-west-1.amazonaws.com/scp/images/scp-logo.png'
+                  className='img-fluid mb-5'
+                  alt='SCP Logo'
+                />
+
+                <form onSubmit={e => handleSubmit(e)}>
+                  <div className='form-group form-group--insertIcon insertIcon-left'>
+                    <input
+                      type='email'
+                      className='form-control input-field font-italic'
+                      placeholder='Email'
+                      name='email'
+                      value={email}
+                      onChange={e => handleChange(e)}
+                      required
+                    />
+                    <i className='far fa-envelope'></i>
+                  </div>
+                  <div className='form-group form-group--insertIcon insertIcon-left'>
+                    <input
+                      type='password'
+                      className='form-control input-field font-italic'
+                      placeholder='Password'
+                      name='password'
+                      value={password}
+                      onChange={e => handleChange(e)}
+                      minLength='6'
+                    />
+                    <i className='fas fa-lock'></i>
+                  </div>
+                  <button type='submit' className='btn btn-themeBlue btn-block'>
+                  Login
+                  </button>
+                </form>
+                <button
+                  className='btn btn-themeBlue btn-block mt-3'
+                  onClick={() => guestLogin()}
+                >
+                Guest login (Demo)
+                </button>
+                <div className='my-4 font-weight-bold'>OR</div>
+                <Link to='/register' className='btn btn-themeBlue btn-block'>
+                Sign up
+                </Link>
+              </div>
             </div>
-            <div className='form-group form-group--insertIcon insertIcon-left'>
-              <input
-                type='password'
-                className='form-control input-field font-italic'
-                placeholder='Password'
-                name='password'
-                value={password}
-                onChange={e => handleChange(e)}
-                minLength='6'
-              />
-              <i className='fas fa-lock'></i>
-            </div>
-            <button type='submit' className='btn btn-themeBlue btn-block'>
-              Login
-            </button>
-          </form>
-          <button
-            className='btn btn-themeBlue btn-block mt-3'
-            onClick={() => guestLogin()}
-          >
-            Guest login (Demo)
-          </button>
-          <div className='my-4 font-weight-bold'>OR</div>
-          <Link to='/register' className='btn btn-themeBlue btn-block'>
-            Sign up
-          </Link>
+          </div>
         </div>
       </div>
     </Fragment>
